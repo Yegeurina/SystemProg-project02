f1ecb5b113bebc77c7037cbe0eb70006bbdc1307
Yegeurina <yegeurinasec@gmail.com>
2021-06-12 08:53:33 +0900

    detach to join

--- server.c
+++ server.c
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
             pthread_mutex_unlock(&mutx);
 
             pthread_create(&t_id, NULL, handle_clnt, (void *)&clnt_sock); //thread
-            pthread_detach(t_id);
+            pthread_join(t_id);
             printf(" Connceted client IP : %s ", inet_ntoa(clnt_adr.sin_addr));
             printf("(%d-%d-%d %d:%d)\n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min); //join time
             printf(" User (%d/100)\n", clnt_cnt);
